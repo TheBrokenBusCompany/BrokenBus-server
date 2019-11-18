63d26864ad7a485949062a691006d3fc62223341
jfaldanam <jfaldanam@gmail.com>
2019-11-18 21:00:33 +0100

    Extract js from html
    Add example of adding a button over the map

---
+++ static/js/map.js
@@ -0,0 +1,10 @@
+// initialize the map
+var map = L.map('map').setView([36.711, -4.422], 13);
+
+// load a tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+        maxZoom: 19,
+        minZoom: 2
+    }).addTo(map);
